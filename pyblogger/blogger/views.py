53e11d85257e684c6b79a9e125531c84f6efd54c
vinay <ghatevinay2@gmail.com>
2025-03-19 00:15:55 +0530

    Simple UI and form added

--- pyblogger/blogger/views.py
+++ pyblogger/blogger/views.py
@@ -1,7 +1,28 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Post
+import datetime
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the blogger index.")
+    return render(request, 'home.html')
+
+def addPost(request):
+    if request.method == 'POST':
+        title = request.POST['title']
+        content = request.POST['content']
+        post = Post.objects.create(title=str(title), text=str(content), date=datetime.datetime.now())
+        return HttpResponse(f'Post added: {title} and id is {post.id}')
+    else:
+        posts = Post.objects.all().values()
+        return render(request, 'home.html', {'posts': posts})
+    
+def getPost(request):
+    if request.method == 'GET':
+        posts = Post.objects.all().values()
+        return render(request, 'home.html', {'posts': posts})
+    else:
+        return HttpResponse('Invalid request method')
+
+
