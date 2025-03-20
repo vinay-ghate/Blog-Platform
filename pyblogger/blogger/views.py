bdc66233bd3bd6a7d0ea0233a0825737ab86a62d
vinay <ghatevinay2@gmail.com>
2025-03-21 01:55:42 +0530

    Added Header, Footer and Made pages more specefic

--- pyblogger/blogger/views.py
+++ pyblogger/blogger/views.py
@@ -1,27 +1,28 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import Post
 import datetime
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'home.html')
+    return render(request, 'posts.html')
 
 def addPost(request):
     if request.method == 'POST':
         title = request.POST['title']
         content = request.POST['content']
         post = Post.objects.create(title=str(title), text=str(content), date=datetime.datetime.now())
-        return HttpResponse(f'Post added: {title} and id is {post.id}')
+        # return HttpResponse(f'Post added: {title} and id is {post.id}')
+        return redirect('/posts/')
     else:
         posts = Post.objects.all().values()
-        return render(request, 'home.html', {'posts': posts})
+        return render(request, 'Addposts.html', {'posts': posts})
     
 def getPost(request):
     if request.method == 'GET':
         posts = Post.objects.all().values()
-        return render(request, 'home.html', {'posts': posts})
+        return render(request, 'posts.html', {'posts': posts[::-1]})
     else:
         return HttpResponse('Invalid request method')
 
